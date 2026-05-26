e19caf492b13a010e2c30f3503a5571971501ffa
raezualways <kragljk09@gmail.com>
2026-05-26 17:00:16 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'game-v1';
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/game.js', '/style.css'])));
-});
-
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
